7d953df7b58d15ab1ac81c8c32ab3579a2959282
Chu-4hun <70685495+Chu-4hun@users.noreply.github.com>
2021-09-15 17:09:44 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      theme: ThemeData(primarySwatch: Colors.green),
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
-        appBar: AppBar(title: Text('Za wardo')),
-        body: const Center(
-          child: Text('Hello World'),
+        body: SafeArea(
+          child: Column(children: const <Widget>[
+            CircleAvatar(
+              radius: 60.0,
+              backgroundColor: Colors.black,
+              backgroundImage: AssetImage('images/koboyashi.jpg'),
+              child: Text('Bruh',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                  )),
+            ),
+            Text(
+              'bruh',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ]),
         ),
       ),
     );
