c968823a7dcc7222a660a4d6297726f3b1b62673
Chu-4hun <70685495+Chu-4hun@users.noreply.github.com>
2021-09-19 15:01:56 +0300

    Added few cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,77 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ));
     return MaterialApp(
+      debugShowCheckedModeBanner: false, //debug bunner
+
       theme: ThemeData(primarySwatch: Colors.green),
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         body: SafeArea(
-          child: Column(children: const <Widget>[
-            CircleAvatar(
-              radius: 60.0,
-              backgroundColor: Colors.black,
-              backgroundImage: AssetImage('images/koboyashi.jpg'),
-              child: Text('Bruh',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.bold,
-                  )),
-            ),
-            Text(
-              'bruh',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 30.0,
-                fontWeight: FontWeight.bold,
+          child: Align(
+            alignment: Alignment.topCenter,
+            child: Column(children: <Widget>[
+              CircleAvatar(
+                radius: 60.0,
+                backgroundColor: Colors.black,
+                backgroundImage: AssetImage('images/koboyashi.jpg'),
               ),
-            ),
-          ]),
+              Text(
+                'Hello',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Raleway',
+                ),
+              ),
+              Text(
+                'I am Dan',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.w500,
+                  fontFamily: 'Raleway',
+                  letterSpacing: 4,
+                ),
+              ),
+              Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5),
+                  color: Colors.white,
+                  child: Row(
+                    children: const [
+                      Icon(
+                        Icons.perm_identity,
+                        size: 50.0,
+                        color: Colors.blueGrey,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        '+7(903)610-11-71',
+                        style: TextStyle(
+                          color: Colors.blueGrey,
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: 'IBM Plex Sans',
+                        ),
+                      ),
+                    ],
+                  ))
+            ]),
+          ),
         ),
       ),
     );
